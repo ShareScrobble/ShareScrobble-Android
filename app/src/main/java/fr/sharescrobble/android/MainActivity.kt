1f518bfa98f516ec0b17e61ac2ba610f35c394c2
Gabriel Dugny <gabriel.dugny@gmail.com>
2021-03-17 15:36:39 +0100

    feat: add friends view

--- app/src/main/java/fr/sharescrobble/android/MainActivity.kt
+++ app/src/main/java/fr/sharescrobble/android/MainActivity.kt
@@ -1,19 +1,26 @@
 package fr.sharescrobble.android
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import fr.sharescrobble.android.auth.AuthService
 import fr.sharescrobble.android.auth.ui.AuthActivity
 import fr.sharescrobble.android.core.Globals
+import fr.sharescrobble.android.main.MyRecyclerViewAdapter
 import fr.sharescrobble.android.network.ApiRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), MyRecyclerViewAdapter.ItemClickListener {
+    var adapter: MyRecyclerViewAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,5 +42,33 @@ class MainActivity : AppCompatActivity() {
                 Log.d(Globals.TAG, response.toString())
             }
         }
+
+        // data to populate the RecyclerView with
+
+        // data to populate the RecyclerView with
+        val data = arrayOf(
+            "1",
+            "2"
+        )
+
+        // set up the RecyclerView
+
+        // set up the RecyclerView
+        val recyclerView = findViewById<RecyclerView>(R.id.rvFriends)
+        val numberOfColumns = 2
+        recyclerView.layoutManager = GridLayoutManager(this, numberOfColumns)
+        adapter = MyRecyclerViewAdapter(this, data)
+        adapter!!.setClickListener(this)
+        recyclerView.adapter = adapter
     }
+
+
+    override fun onItemClick(view: View?, position: Int) {
+        Log.i(
+            "TAG",
+            "You clicked number " + this.adapter!!.getItem(position) + ", which is at cell position " + position
+        );
+    }
+
+
 }
--- app/src/main/java/fr/sharescrobble/android/auth/ui/AuthActivity.kt
+++ app/src/main/java/fr/sharescrobble/android/auth/ui/AuthActivity.kt
@@ -2,10 +2,14 @@ package fr.sharescrobble.android.auth.ui
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import fr.sharescrobble.android.R
 import fr.sharescrobble.android.auth.AuthService
 import fr.sharescrobble.android.core.Globals
+
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,12 +21,20 @@ class AuthActivity : AppCompatActivity() {
         Log.d(Globals.TAG, "Auth")
         Log.d(Globals.TAG, base64Token.toString());
 
-        if (base64Token == null) {
-            AuthService.initLogin()
-            return
-        }
+        val button: Button = this.findViewById(R.id.buttonLogin)
+        button.setOnClickListener(object : OnClickListener {
+            override fun onClick(v: View?) {
+                if (base64Token == null) {
+                    AuthService.initLogin()
+                    return
+                }
 
-        Log.d(Globals.TAG, base64Token.toString())
-        AuthService.finishLogin(base64Token.toString())
+            }
+        })
+
+        if(base64Token != null) {
+            Log.d(Globals.TAG, base64Token.toString())
+            AuthService.finishLogin(base64Token.toString())
+        }
     }
 }
